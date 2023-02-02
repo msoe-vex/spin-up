34fa64cfc7df119e9d9351c7c28f5b1d013349fc
Dhanish Pari <dhanishpari@gmail.com>
2023-02-02 23:33:22 +0000

    changed robot motors and now pragma doesnt work

--- include/constant/motor_constants.h
+++ include/constant/motor_constants.h
@@ -1,26 +1,12 @@
 #pragma once
 #include <vector>
 
-#include "hardware/pros_motor.h"
-#include "interface/motor.h"
 #include "main.h"
 
 /**
  * Factory functions for retriving Motors on the robot.
  */
 namespace constant {
-struct DrivetrainMotors {
-  // declared in clockwise fashion
-  Motor front_right_motor;
-  Motor back_right_motor;
-  Motor back_left_motor;
-  Motor front_left_motor;
-};
-
-DrivetrainMotors GetDrivetrainMotors();
-// interface::MotorAndEncoder GetShooterMotor();
-// interface::Motor GetTransferMotor();
-
 const pros::motor_gearset_e_t kDrivetrainGearset = pros::E_MOTOR_GEAR_BLUE;
 const std::vector<bool> kDrivetrainReverse = {false, true};
 // {top motor, bottom motor}
