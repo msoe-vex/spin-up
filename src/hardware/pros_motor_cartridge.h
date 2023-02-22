4e9fa86575b1492f734e89881b1111cf75029741
Dhanish Pari <dhanish@Dhanishs-MacBook-Pro.local>
2023-02-22 12:47:51 -0600

    Added red motor

--- src/hardware/pros_motor_cartridge.h
+++ src/hardware/pros_motor_cartridge.h
@@ -6,7 +6,8 @@ namespace hardware {
 enum class ProsMotorCartridge {
   // equal to pros value to enable casting
   kBlueCartridge = pros::E_MOTOR_GEAR_BLUE,   // 600 rpm
-  kGreenCartridge = pros::E_MOTOR_GEAR_GREEN  // 200 rpm
+  kGreenCartridge = pros::E_MOTOR_GEAR_GREEN,  // 200 rpm
+  kRedCartridge = pros::E_MOTOR_GEAR_RED  // 100 rpm
 };
 
 /**
